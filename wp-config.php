3c97451ac02d6a810980e1d9f61a9d4bf9bc11b8
JaidynReiman <jrconway3@gmail.com>
2026-05-18 19:27:40 -0500

    * Copilot Comment Fixes

--- wp-config.php
+++ wp-config.php
@@ -104,7 +104,7 @@ define( 'AUTOSAVE_INTERVAL', 300 );
 define( 'WP_POST_REVISIONS', 5 );
 define( 'EMPTY_TRASH_DAYS', 7 );
 define( 'WP_CRON_LOCK_TIMEOUT', 120 );
-define('WP_AUTO_UPDATE_CORE', 'minor');
+define( 'WP_AUTO_UPDATE_CORE', 'minor' );
 
 if ( ! defined( 'ABSPATH' ) ) {
     define( 'ABSPATH', __DIR__ . '/' );
